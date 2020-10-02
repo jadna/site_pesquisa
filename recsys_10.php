0f6ec213e333b96f1256faeea10da143c9445c24
jadna <jadna.ac@gmail.com>
2020-10-02 11:39:04 -0300

    avaliacao

---
+++ recsys_10.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <style>
+    /* Always set the map height explicitly to define the size of the div
+     * element that contains the map. */
+    #map {
+      height: 100%;
+    }
+    /* Optional: Makes the sample page fill the window. */
+    html, body {
+      height: 100%;
+      margin: 0;
+      padding: 0;
+    }
+  </style>
+
+  <title>Recomendação de Pontos de Interesse para Grupos de Indivíduos</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+ 
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Group Systems Recommendation for POI</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <!--li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#pesquisa">Pesquisa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#instrucoes">Instruções</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="cadastro.html"> Cadastro</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="login.html"> Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="https://www.mapdevelopers.com/what-is-my-address.php" target="_blank"> Coordenada</a>
+          </li-->
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white" style="background-image: url(background.jpg);">
+    <div class="container text-center">
+      <h1>Avaliação das Recomendações</h1>
+      <h2>Recomendação de Pontos de Interesse para Grupos de Indivíduos</h2>
+      <p class="lead">Avalie os pontos de interesse recomendados para você</p>
+    </div>
+  </header>
+
+  <section id="avaliacao">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeDyZuTdPrXgMvX7_9bKIzvvvUYMPsEH2GFVkSiicUMzliK9Q/viewform?embedded=true" width="640" height="3697" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+
+
+
+
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Jadna Almeida</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+  <!-- The core Firebase JS SDK is always required and must be listed first -->
+  <script src="https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js"></script>
+
+  <!-- TODO: Add SDKs for Firebase products that you want to use
+      https://firebase.google.com/docs/web/setup#available-libraries -->
+  <script src="https://www.gstatic.com/firebasejs/7.20.0/firebase-analytics.js"></script>
+
+  <script>
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    /*var firebaseConfig = {
+      apiKey: "AIzaSyCQ1dYNC9ZQNTJJ1PM4u_su5fOxaLjUM_U",
+      authDomain: "grs-poi.firebaseapp.com",
+      databaseURL: "https://grs-poi.firebaseio.com",
+      projectId: "grs-poi",
+      storageBucket: "grs-poi.appspot.com",
+      messagingSenderId: "128070969039",
+      appId: "1:128070969039:web:d59e441d90f8d40af5b9ba",
+      measurementId: "G-1V4QD5R5XJ"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();*/
+  </script>
+
+</body>
+
+</html>
